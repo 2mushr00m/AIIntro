831e20008f36a3fcea7253a478a0acb28b0d31c9
김소현 <sally1196420@gmail.com>
2024-05-15 19:27:09 +0900

    인공지능개론 HW#1

---
+++ HW#1/HW#1.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from dataset.mnist import load_mnist
+from common.multi_layer_net import MultiLayerNet
+from common.util import shuffle_dataset
+from common.trainer import Trainer
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
+
+# 결과를 빠르게 얻기 위해 훈련 데이터를 줄임
+x_train = x_train[:500]
+t_train = t_train[:500]
+
+# 검증 데이터 분리
+validation_rate = 0.20
+validation_num = int(x_train.shape[0] * validation_rate)
+x_train, t_train = shuffle_dataset(x_train, t_train)
+x_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
+x_train = x_train[validation_num:]
+t_train = t_train[validation_num:]
+
+
+def __train(lr, weight_decay, hidden_size, hidden_layer_size, epocs=10000):
+    network = MultiLayerNet(input_size=784, hidden_size_list=[hidden_size] * hidden_layer_size,
+                            output_size=10, weight_decay_lambda=weight_decay)
+    trainer = Trainer(network, x_train, t_train, x_val, t_val,
+                      epochs=epocs, mini_batch_size=100,
+                      optimizer='sgd', optimizer_param={'lr': lr}, verbose=False)
+    trainer.train()
+
+    return trainer.test_acc_list, trainer.train_acc_list
+
+
+# 하이퍼파라미터의 랜덤 탐색
+optimization_trial = 100
+results_val = {}
+results_train = {}
+for _ in range(optimization_trial):
+    # 탐색할 하이퍼파라미터 범위 지정
+    weight_decay = 10 ** np.random.uniform(-8, -4)
+    lr = np.random.uniform(0.001, 0.019)
+    hidden_size = np.random.randint(5, 16)
+    hidden_layer_size = 3
+    # ================================================
+
+    val_acc_list, train_acc_list = __train(lr, weight_decay, hidden_size, hidden_layer_size)
+    print("val acc:" + str(val_acc_list[-1]) + " | lr:" + str(lr) + ", weight decay:" + str(weight_decay))
+    key = "lr:" + str(lr) + ", weight decay:" + str(weight_decay)
+    results_val[key] = val_acc_list
+    results_train[key] = train_acc_list
+
+# 그래프 그리기
+print("=========== Hyper-Parameter Optimization Result ===========")
+graph_draw_num = 20
+col_num = 5
+row_num = int(np.ceil(graph_draw_num / col_num))
+i = 0
+
+for key, val_acc_list in sorted(results_val.items(), key=lambda x:x[1][-1], reverse=True):
+    print("Best-" + str(i+1) + "(val acc:" + str(val_acc_list[-1]) + ") | " + key)
+
+    plt.subplot(row_num, col_num, i+1)
+    plt.title("Best-" + str(i+1))
+    plt.ylim(0.0, 1.0)
+    if i % 5: plt.yticks([])
+    plt.xticks([])
+    x = np.arange(len(val_acc_list))
+    plt.plot(x, val_acc_list)
+    plt.plot(x, results_train[key], "--")
+    i += 1
+
+    if i >= graph_draw_num:
+        break
+
+plt.show()
