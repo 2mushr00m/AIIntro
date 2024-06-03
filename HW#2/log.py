ada6332646278a82b7dd5e00b266c62fe424f696
2mushr00m <sally1196420@gmail.com>
2024-06-03 23:34:24 +0900

    HW#2

---
+++ HW#2/log.py
@@ -0,0 +1,21 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\SoHyun\OneDrive - 강남대학교\University\2024년도 1학기\인공지능개론\prj\HW#2\HW#2.py
+train acc, test acc | 0.10441666666666667, 0.1028
+train acc, test acc | 0.9366666666666666, 0.9388
+train acc, test acc | 0.9607833333333333, 0.9614
+train acc, test acc | 0.9723, 0.9699
+train acc, test acc | 0.9797833333333333, 0.9762
+train acc, test acc | 0.9844, 0.9806
+train acc, test acc | 0.9842666666666666, 0.98
+train acc, test acc | 0.9887833333333333, 0.9832
+train acc, test acc | 0.9904833333333334, 0.9829
+train acc, test acc | 0.992, 0.9851
+train acc, test acc | 0.99305, 0.9853
+train acc, test acc | 0.9931333333333333, 0.9852
+train acc, test acc | 0.9940333333333333, 0.9861
+train acc, test acc | 0.9957833333333334, 0.9863
+train acc, test acc | 0.9943333333333333, 0.9844
+train acc, test acc | 0.9951166666666666, 0.9867
+train acc, test acc | 0.9968166666666667, 0.9859
