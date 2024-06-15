7c2fe803fa909a58eae1383c326fd0573c9691f3
2mushr00m <sally1196420@gmail.com>
2024-06-15 22:38:49 +0900

    데이터 로드 코드 추가

--- HW#2/log.py
+++ HW#2/log.py
@@ -1,24 +1,42 @@
 Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+
 = RESTART: C:\Users\SoHyun\OneDrive - 강남대학교\University\2024년도 1학기\인공지능개론\HW\HW#2\HW#2.py
-train acc, test acc | 0.2156, 0.216
-train acc, test acc | 0.8877166666666667, 0.8769
-train acc, test acc | 0.9130333333333334, 0.89
-train acc, test acc | 0.90875, 0.8821
-train acc, test acc | 0.9243666666666667, 0.8931
-train acc, test acc | 0.9380333333333334, 0.8999
-train acc, test acc | 0.9458666666666666, 0.9049
-train acc, test acc | 0.9493666666666667, 0.9039
-train acc, test acc | 0.9526166666666667, 0.9034
-train acc, test acc | 0.9637833333333333, 0.9097
-train acc, test acc | 0.9645833333333333, 0.9059
-train acc, test acc | 0.96835, 0.9103
-train acc, test acc | 0.9648333333333333, 0.8995
-train acc, test acc | 0.9756833333333333, 0.9094
-train acc, test acc | 0.9822666666666666, 0.9105
-train acc, test acc | 0.9792333333333333, 0.9067
-train acc, test acc | 0.9787666666666667, 0.9071
-train acc, test acc | 0.9837333333333333, 0.91
-train acc, test acc | 0.9833333333333333, 0.9066
-train acc, test acc | 0.98665, 0.9086
+Downloading train-images-idx3-ubyte.gz ... 
+Done
+Downloading train-labels-idx1-ubyte.gz ... 
+Done
+Downloading t10k-images-idx3-ubyte.gz ... 
+Done
+Downloading t10k-labels-idx1-ubyte.gz ... 
+Done
+Converting train-images-idx3-ubyte.gz to NumPy Array ...
+Done
+Converting train-labels-idx1-ubyte.gz to NumPy Array ...
+Done
+Converting t10k-images-idx3-ubyte.gz to NumPy Array ...
+Done
+Converting t10k-labels-idx1-ubyte.gz to NumPy Array ...
+Done
+Creating pickle file ...
+Done!
+train acc, test acc | 0.35988333333333333, 0.3556
+train acc, test acc | 0.88785, 0.8727
+train acc, test acc | 0.9098833333333334, 0.8874
+train acc, test acc | 0.9282666666666667, 0.9026
+train acc, test acc | 0.9373833333333333, 0.9051
+train acc, test acc | 0.9434666666666667, 0.9048
+train acc, test acc | 0.9512333333333334, 0.9099
+train acc, test acc | 0.96075, 0.9142
+train acc, test acc | 0.96005, 0.9047
+train acc, test acc | 0.965, 0.9113
+train acc, test acc | 0.9605666666666667, 0.9039
+train acc, test acc | 0.97385, 0.9121
+train acc, test acc | 0.973, 0.9067
+train acc, test acc | 0.9802666666666666, 0.914
+train acc, test acc | 0.97465, 0.9069
+train acc, test acc | 0.9807166666666667, 0.9053
+train acc, test acc | 0.9837333333333333, 0.9126
+train acc, test acc | 0.9858666666666667, 0.9081
+train acc, test acc | 0.9842333333333333, 0.9092
+train acc, test acc | 0.97955, 0.9039
